3d24e164f7637d1dff8baee865737f02a5a633de
silentkuan <jiunying2000kjy@gmail.com>
2022-03-13 20:46:58 +0800

    -m

---
+++ cms/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS</title>
+    
+    <!--  ICON -->
+    <link rel="stylesheet" href="../fontawesome/css/all.css" >
+
+    <!-- CSS -->
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="container">
+        <aside>
+            <div class="top">
+                <div class="logo">
+                    <img src="images/logo.png" alt="logo">
+                    <h2>EGA <span class="danger">TOR</span></h2>
+                </div>
+                <div class="close" id="close-btn">
+                    <i class="fa-solid fa-xmark"></i>
+                </div>
+            </div>
+
+            <div class="sidebar">
+                <a href="">
+                <i class="fa-solid fa-chart-tree-map"></i>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="">
+                <i class="fa-solid fa-border-all"></i>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="">
+                <i class="fa-solid fa-border-all"></i>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="">
+                <i class="fa-solid fa-border-all"></i>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="">
+                <i class="fa-solid fa-border-all"></i>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="">
+                <i class="fa-solid fa-border-all"></i>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="">
+                <i class="fa-solid fa-border-all"></i>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="">
+                <i class="fa-solid fa-border-all"></i>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="">
+                <i class="fa-solid fa-border-all"></i>
+                    <h3>Dashboard</h3>
+                </a>
+            </div>
+        </aside>
+    </div>
+    
+</body>
+</html>
